778067aa1016b92671b7eeaadb50307dfea866b0
kam3r0nz <kam.hungerford@gmail.com>
2020-12-13 01:22:24 -0600

    Created new animal page

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,4 +4,21 @@ class AnimalsController < ApplicationController
     def index
         @animals = Animal.all
     end
+
+    def new
+        @animal = Animal.new
+    end
+
+    def create
+        @animal = Animal.new(animal_params)
+        if @animal.save
+            redirect_to animal_path(@animal)
+        end
+    end
+
+    private
+
+    def pet_params
+        params.require(:pet).permit(:name, :age, :color, :species, :image)
+    end
 end
