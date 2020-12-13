0bfedc2f9dc1ca02fa45a36984b06ef5d26cab1b
kam3r0nz <kam.hungerford@gmail.com>
2020-12-13 00:24:10 -0600

    Added first and last name columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_052312) do
+ActiveRecord::Schema.define(version: 2020_12_13_062256) do
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2020_12_13_052312) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "veterinarians", force: :cascade do |t|
