45f154f8fff297cd3cd260630be06b7d502e6fbf
kam3r0nz <kam.hungerford@gmail.com>
2020-12-12 23:49:56 -0600

    Created initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
+
+  resources :animals
+  resources :appointments
+  resources :veterinarians
+  resources :services
 end
