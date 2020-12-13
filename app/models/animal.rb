3f578fe0b91b215c427dd3d3da4794428c592c9a
kam3r0nz <kam.hungerford@gmail.com>
2020-12-12 23:43:31 -0600

    Established associations

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,5 @@
 class Animal < ApplicationRecord
   belongs_to :user
-  has_many :appointments, through: :veterinarian
+  has_many :appointments
+  has_many :veterinarian, through: :appointments
 end
