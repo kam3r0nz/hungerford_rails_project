1970f3fc3fda57a9a614f70bf56956cc683c00bb
kam3r0nz <kam.hungerford@gmail.com>
2020-12-13 17:03:20 -0600

    Added helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
+    helper_method :current_user, :require_login
 
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
