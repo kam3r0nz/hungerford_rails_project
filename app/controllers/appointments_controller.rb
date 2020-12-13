bc879300043cd94d7d9c65de9dafcd5632e230cd
kam3r0nz <kam.hungerford@gmail.com>
2020-12-13 15:33:12 -0600

    Created appointments index

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,7 @@
 class AppointmentsController < ApplicationController
     before_action :require_login
     def index
+        @appointments = Appointment.all
     end
     
     def new
@@ -8,6 +9,16 @@ class AppointmentsController < ApplicationController
     end
 
     def create
+        @appointment = Appointment.new(appointment_params)
+        if @appointment.save
+            redirect_to appointment_path(@appointment)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @appointment = Appointment.find_by(id: params[:id])
     end
 
     def edit
@@ -17,5 +28,9 @@ class AppointmentsController < ApplicationController
     end
 
     def destroy
+    end
+
+    def appointment_params
+        params.require(:appointment).permit(:date, :animal_id, :veterinarian_id, :service_id)
     end
 end
