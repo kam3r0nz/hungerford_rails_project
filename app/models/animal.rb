5c8191eebd56a2dfe78688513cb1f9a782bce7f0
kam3r0nz <kam.hungerford@gmail.com>
2020-12-17 16:34:58 -0600

    Made image upload optional

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,5 @@
 class Animal < ApplicationRecord
+  validates :name, presence: true
   belongs_to :user
   has_many :appointments, through: :veterinarians
   has_many :veterinarian, through: :appointments
