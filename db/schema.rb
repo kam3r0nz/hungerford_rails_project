a767859f05b1c3b703b2568c031280ea814d4ee1
kam3r0nz <kam.hungerford@gmail.com>
2020-12-17 12:20:55 -0600

    Added admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_032859) do
+ActiveRecord::Schema.define(version: 2020_12_17_181852) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,14 +38,6 @@ ActiveRecord::Schema.define(version: 2020_12_17_032859) do
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "name"
-    t.string "password"
-    t.string "admin_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "animals", force: :cascade do |t|
@@ -88,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_032859) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.boolean "admin", default: false
   end
 
   create_table "veterinarians", force: :cascade do |t|
