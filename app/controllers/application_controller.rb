18e1118f8f8b5521abca6add088e7bc37188caf3
kam3r0nz <kam.hungerford@gmail.com>
2020-12-17 12:45:26 -0600

    Added admin functionality and helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
+    def admin?
+        if current_user.admin == true
+            @admin ||= User.find(session[:user_id]) if session[:user_id]
+        end
+    end
+
     def require_login
         unless current_user
             redirect_to login_path
