2d89cdaa4f67cece0c7b5f053f6ac75e2612077a
kam3r0nz <kam.hungerford@gmail.com>
2020-12-17 13:08:03 -0600

    Created logic for new veterinarian

--- app/controllers/veterinarians_controller.rb
+++ app/controllers/veterinarians_controller.rb
@@ -2,4 +2,19 @@ class VeterinariansController < ApplicationController
     def new
         @veterinarian = Veterinarian.new
     end
+
+    def create
+        @veterinarian = Veterinarian.new(veterinarian_params)
+        if @veterinarian.save
+            redirect_to veterinarian_path(@veterinarian)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def veterinarian_params
+        params.require(:veterinarian).permit(:name)
+    end
 end
