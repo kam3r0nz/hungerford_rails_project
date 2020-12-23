27e79822bdad86c7f53706a9c20d1a71bf947e61
kam3r0nz <kam.hungerford@gmail.com>
2020-12-23 16:35:28 -0600

    Added the animal appointment index page back

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   match '/auth/google_oauth2/callback', to: 'sessions#create_with_google', via: [:get, :post]
   delete 'logout', to: 'sessions#destroy'
 
+  get 'users/:id/animals/:id/appointments', to: 'appointments#animal_appointment_index', as: 'user_animal_appointments'
+
   resources :admin
   resources :animals
   resources :appointments
