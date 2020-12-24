626f09b003dd646ae0aee0ccaa48dc0742f31c6e
kam3r0nz <kam.hungerford@gmail.com>
2020-12-23 18:19:22 -0600

    Fixed model relationships

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,7 +1,12 @@
 class Animal < ApplicationRecord
   validates :name, presence: true
   belongs_to :user
-  has_many :appointments, through: :veterinarians
-  has_many :veterinarian, through: :appointments
+  has_many :appointments
+  has_many :veterinarians, through: :appointments
   has_one_attached :image
 end
+
+
+#animals/6/appointments/new
+#v/5/apointments/new
+#v/animals/4/appointments/new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_many :animals
-    has_many :appointments
     validates :name, presence: true
     validates :email, uniqueness: true
     has_secure_password
